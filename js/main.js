47dee797280f27d9034fdb9d523f47611350c71b
Rodrigo-363 <rodrigo.tecnologia@troupebrasil.com.br>
2024-01-20 17:19:35 -0300

    Atualizando layout

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const ctx = document.getElementById('myChart');
-  const labels = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+  const labels = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
   const data = [12.82, 12.99, 13.26, 13.36, 13.47, 13.52, 13.57, 13.53, 13.42, 13.40, 13.29, 13.12];
 
   new Chart(ctx, {
@@ -8,13 +8,13 @@ const ctx = document.getElementById('myChart');
       labels: labels,
       datasets: [{
         label: '# of Votes',
-        backgroundColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(75, 192, 192, 0.7)',
         data: data,
         borderWidth: 2
       }]
     },
     options: {
-      scales: {
+      cales: {
         y: {
           beginAtZero: true
         }
